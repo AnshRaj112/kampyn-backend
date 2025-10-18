76fa27fa410ac161279d69a8a58d5a7a736cf399
AnshRaj112 <anshraj112@gmail.com>
2025-10-18 23:48:00 +0530

    Fixing path of account users in User.test

--- test/unit/models/User.test.js
+++ test/unit/models/User.test.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../../models/account/User');
+const User = require('../../../models/account/User');
 
 describe('User Model', () => {
   beforeAll(async () => {
